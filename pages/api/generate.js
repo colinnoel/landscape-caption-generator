106bc9b32a361cc54c48d27cd6d7b60ded852f52
colinnoel <cn@colinnoel.com>
2022-12-05 23:46:34 -0800

    debug

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,18 +9,23 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const { animal, temperature } = req.body;
 
-  // Limit the length of the landscape description to 100 characters.
-  const landscapeDescription = animal.substring(0, 100);
+  let landscapeDescription;
+  if (animal && animal.length > 0) {
+    // Limit the length of the landscape description to 100 characters.
+    landscapeDescription = animal.substring(0, 100);
 
-  // Generate the prompt for the language model using the limited landscape description.
-  const prompt = generatePrompt(landscapeDescription);
+    // Generate the prompt for the language model using the limited landscape description.
+    const prompt = generatePrompt(landscapeDescription);
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt,
-    temperature,
-  });
-  res.status(200).json({ result: completion.data.choices[0].text });
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      temperature,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } else {
+    res.status(400).json({ error: "Please provide a landscape description." });
+  }
 }
 
 function generatePrompt(animal) {
