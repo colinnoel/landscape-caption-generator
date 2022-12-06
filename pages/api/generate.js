efc7771f385187c17339359367a5596fc34c26a1
colinnoel <cn@colinnoel.com>
2022-12-05 19:28:55 -0800

    api changes

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,8 @@
 import { Configuration, OpenAIApi } from "openai";
 
+const apiKey = process.env.OPENAI_API_KEY;
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey,
 });
 const openai = new OpenAIApi(configuration);
 
