8b062473f59106a37e4e451d2735ee972f900d0f
colinnoel <cn@colinnoel.com>
2022-12-06 23:49:12 -0800

    anim animalinput

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        prompt,
+        animal: animalInput, // Include the animal in the request body
         temperature, // Include the temperature in the request body
       }),
     });
